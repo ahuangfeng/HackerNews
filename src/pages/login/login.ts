154d4be09cbaaa687e6f3736d9704b2e3a9ac24f
MasterofDragons <Jiahui1@hotmail.es>
2019-08-14 19:32:07 +0200

    feat: Main Page Update
    - Login created
    - Calls authentication-service methods
    - Error message

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,41 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Loading, AlertController, LoadingController } from 'ionic-angular';
+import { AuthService } from '../../services/auth-service';
+import { text } from '@angular/core/src/render3/instructions';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loading: Loading;
+  registerCredentials = { username:'', password:'' };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private auth: AuthService, private alertControl: AlertController) {
 
   }
 
+  //Login from the Login Page
+  public login(){
+    //Calling Login Method from our Authentication Service
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if(allowed){
+        console.log("Logged In");
+      } else {
+        error => {
+          this.showError(error);
+        }
+      }
+    })
+  }
+
+  showError(error) {
+    this.loading.dismiss;
+
+    let alert = this.alertControl.create({
+      title: 'Fail',
+      subTitle: error,
+      buttons: ['OK']
+    });
+  }
 }
