5d3825cb993d61862bdfff453f134ab4ed6e7454
MasterofDragons <Jiahui1@hotmail.es>
2019-08-15 10:36:29 +0200

    feat: Advanced registration form
    - Mapped button functionalities
    - Loading Screen
    - Error prompt
    - Some comments +

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, Loading, AlertController, LoadingController } from 'ionic-angular';
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
 import { AuthService } from '../../services/auth-service';
 import { text } from '@angular/core/src/render3/instructions';
 
@@ -11,31 +11,45 @@ export class LoginPage {
   loading: Loading;
   registerCredentials = { username:'', password:'' };
 
-  constructor(public navCtrl: NavController, private auth: AuthService, private alertControl: AlertController) {
+  constructor(public navCtrl: NavController, private auth: AuthService, private alertControl: AlertController, private loadingControl: LoadingController) {
 
   }
 
   //Login from the Login Page
   public login(){
+    this.showLoading();
+
     //Calling Login Method from our Authentication Service
     this.auth.login(this.registerCredentials).subscribe(allowed => {
       if(allowed){
         console.log("Logged In");
+
+        //Setting the root page as Home Page
+        //this.navCtrl.setRoot();
       } else {
-        error => {
-          this.showError(error);
-        }
+          this.showError("Access Denied");
+          console.log("ERROR");
       }
-    })
+    });
+  }
+
+  showLoading() {
+    this.loading = this.loadingControl.create({
+      content: 'Please wait...', duration: 150
+    });
+    this.loading.present();
   }
 
   showError(error) {
-    this.loading.dismiss;
+    this.loading.present();
 
     let alert = this.alertControl.create({
-      title: 'Fail',
+      title: 'Login failed',
       subTitle: error,
+      message: 'Input your username and password',
       buttons: ['OK']
     });
+
+    alert.present();
   }
 }
--- src/services/auth-service.ts
+++ src/services/auth-service.ts
@@ -14,15 +14,37 @@ export class User {
 }
 
 
+//Replace Observable by an HTTPS call in the future
 
 @Injectable()
 export class AuthService{
+    currentUser: User;
+    
     public login(credentials){
         if (credentials.username === null || credentials.password === null) {
             return Observable.throw("Please insert credentials");
         } else {
-            //TODO: Make a request to your backend to make a real check!
+            return Observable.create(observer => {
+                let access = (credentials.username === "user" && credentials.password === "psw");
+                //TODO: Make a request to your backend to make a real check!
+                this.currentUser = new User('Timmy', 'timmy@gmail.com');
+                observer.next(access);
+                observer.complete();
+            })
+            
             console.log("Logged In");
         }
+    }
+
+    public getUserInfo(): User {
+        return this.currentUser;
+    }
+
+    public logout() {
+        return Observable.create(observer => {
+            this.currentUser = null;
+            observer.next(true);
+            observer.complete();
+        });
     }
 }
