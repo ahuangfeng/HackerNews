ae9c26851054d7646af7be9607b1c097d1103ad4
MasterofDragons <Jiahui1@hotmail.es>
2019-08-21 09:41:36 +0200

    feat: Tabs navigation available

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, ViewChild } from '@angular/core';
-import { Nav, Platform } from 'ionic-angular';
+import { Nav, Platform, Tabs } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { LoginPage } from '../pages/login/login';
 import { HomePage } from '../pages/homepage/homepage';
 import { ListPage } from '../pages/list/list';
+
+import { Tab1Page } from '../pages/tab1/tab1';
+import { Tab2Page } from '../pages/tab2/tab2';
+import { Tab3Page } from '../pages/tab3/tab3';
 
 @Component({
   templateUrl: 'app.html'
@@ -18,9 +22,13 @@ export class MyApp {
 
   pages: Array<{title: string, component: any}>;
 
+  tab1Root = Tab1Page;
+  tab2Root = Tab2Page;
+  tab3Root = Tab3Page;
+
   constructor(public platform: Platform, public statusBar: StatusBar, public splashScreen: SplashScreen) {
     this.initializeApp();
-
+    
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'HomePage', component: HomePage },
@@ -42,5 +50,6 @@ export class MyApp {
     // Reset the content nav to have just this page
     // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
+    //this.nav.push(page.component);
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+//Declarations - Compiler
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { ListPage } from '../pages/list/list';
-import { AuthService} from '../services/auth-service';
+import { Tab1Page } from '../pages/tab1/tab1';
+import { Tab2Page } from '../pages/tab2/tab2';
+import { Tab3Page } from '../pages/tab3/tab3';
 
+//Services - Provider
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { AuthService} from '../services/auth-service';
+
 
 @NgModule({
   declarations: [
     MyApp,
     LoginPage,
-    ListPage
+    ListPage,
+    Tab1Page,
+    Tab2Page,
+    Tab3Page
   ],
   imports: [
     BrowserModule,
@@ -24,7 +33,10 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     LoginPage,
-    ListPage
+    ListPage,
+    Tab1Page,
+    Tab2Page,
+    Tab3Page
   ],
   providers: [
     StatusBar,
