026d05159dbdfea96330eea91b1e32627076ff08
MasterofDragons <Jiahui1@hotmail.es>
2019-08-25 07:31:50 +0200

    chore: New declarations & provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,45 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import {HttpClientModule} from '@angular/common/http';
 
 //Declarations - Compiler
 import { MyApp } from './app.component';
 import { LoginPage } from '../pages/login/login';
 import { ListPage } from '../pages/list/list';
-import { Tab1Page } from '../pages/tab1/tab1';
-import { Tab2Page } from '../pages/tab2/tab2';
-import { Tab3Page } from '../pages/tab3/tab3';
+import { NewsPage } from '../pages/news/news';
 
 //Services - Provider
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { AuthService} from '../services/auth-service';
-
+import { AuthService } from '../services/auth-service';
+import { NewsService } from '../services/news-service';
 
 @NgModule({
   declarations: [
     MyApp,
     LoginPage,
-    ListPage
+    ListPage,
+    NewsPage
   ],
   imports: [
+    HttpClientModule,
     BrowserModule,
-    IonicModule.forRoot(MyApp),
+    IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     LoginPage,
-    ListPage
+    ListPage,
+    NewsPage
   ],
   providers: [
     StatusBar,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler},
-    AuthService
+    AuthService,
+    NewsService
   ]
 })
 export class AppModule {}
