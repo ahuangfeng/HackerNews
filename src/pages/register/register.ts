022e14e78cd989b36c91ebaab39422fc4c77ce71
MasterofDragons <Jiahui1@hotmail.es>
2019-08-26 12:42:50 +0200

    feat: RegisterPage added

---
+++ src/pages/register/register.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams , AlertController} from 'ionic-angular';
+import { AuthService } from '../../services/auth-service';
+
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+  createOK = false;
+  registerCredentials = { username:'', password:''};
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertControl: AlertController, private auth: AuthService) {
+    
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+
+  public register() {
+
+  }
+
+  showError(error) {
+    let alert = this.alertControl.create({
+      title: 'Registration Failed',
+      subTitle: error,
+      message: 'Enter all fields',
+      buttons: ['OK']
+    });
+
+    alert.present();
+  }
+}
