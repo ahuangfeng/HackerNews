fc64fda5a1835953feaa0c4e83375a595f2a9fbd
MasterofDragons <Jiahui1@hotmail.es>
2019-09-03 12:42:17 +0200

    feat: Profile news

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { NewsPage } from '../news/news';
+import { NewsService } from '../../services/news-service';
+
 @IonicPage()
 @Component({
   selector: 'page-profile',
   templateUrl: 'profile.html',
 })
 export class ProfilePage {
+  public userNews = [];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private newsService: NewsService) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad ProfilePage');
+    this.newsService.getNews().subscribe((response) => {this.userNews = response});
+  }
+
+  goToNewsPage(news) {
+    this.navCtrl.push(NewsPage, { newsDetails: news})
   }
 
   post() {
